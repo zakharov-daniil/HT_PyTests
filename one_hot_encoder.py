43c363d1956f5946621eb8d992e614042b714ab7
zakharov-daniil <65117660+zakharov-daniil@users.noreply.github.com>
2021-12-01 19:03:57 +0300

    Update one_hot_encoder.py

--- one_hot_encoder.py
+++ one_hot_encoder.py
@@ -25,8 +25,6 @@ def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
 
 
 if __name__ == '__main__':
-    from pprint import pprint
-
     cities = ['Moscow', 'New York', 'Moscow', 'London']
     exp_transformed_cities = [
         ('Moscow', [0, 0, 1]),
@@ -36,4 +34,3 @@ if __name__ == '__main__':
     ]
     transformed_cities = fit_transform(cities)
     assert transformed_cities == exp_transformed_cities
-    
