cf63bf7a6034133ee02d308d7434ad2594c51dff
zakharov-daniil <65117660+zakharov-daniil@users.noreply.github.com>
2021-12-02 09:57:58 +0300

    Update test_u_fit.py

--- test_u_fit.py
+++ test_u_fit.py
@@ -4,24 +4,6 @@ from one_hot_encoder import fit_transform
 
 class TestFitTransform(unittest.TestCase):
 
-    def setUp(self):
-        self.texts = [
-            ['Moscow', 'New York', 'Moscow', 'London'],
-            ['hello', 'world'],
-            ['red', 'red', 'green'],
-            ['123']
-        ]
-        self.expected = [
-            [('Moscow', [0, 0, 1]), ('New York', [0, 1, 0]), ('Moscow', [0, 0, 1]), ('London', [1, 0, 0])],
-            [('hello', [0, 1]), ('world', [1, 0])],
-            [('red', [0, 1]), ('red', [0, 1]), ('green', [1, 0])],
-            [('123', [1])]
-        ]
-
-    def tearDown(self):
-        self.texts = []
-        self.expected = []
-
     def test_none(self):
         with self.assertRaises(TypeError):
             result = fit_transform()
@@ -32,11 +14,18 @@ class TestFitTransform(unittest.TestCase):
         self.assertEqual(result, [])
 
     def test_texts(self):
-        for i in range(4):
-            text = self.texts[i]
-            result = fit_transform(text)
+        texts = [
+            (['Moscow', 'New York', 'Moscow', 'London'],
+             [('Moscow', [0, 0, 1]), ('New York', [0, 1, 0]), ('Moscow', [0, 0, 1]), ('London', [1, 0, 0])]),
+            (['hello', 'world'], [('hello', [0, 1]), ('world', [1, 0])]),
+            (['red', 'red', 'green'], [('red', [0, 1]), ('red', [0, 1]), ('green', [1, 0])]),
+            (['123'], [('123', [1])])
+        ]
+
+        for text in texts:
+            result = fit_transform(text[0])
             self.assertIsNotNone(result)
-            self.assertEqual(self.expected[i], result)
+            self.assertEqual(result, text[1])
 
 
 if __name__ == '__main__':
